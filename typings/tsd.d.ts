0b7e077a10b105dd213a63b4479202ed7a91240b
Pritchard, Michael <Michael.Pritchard@nortechonline.co.uk>
2016-02-02 09:34:28 +0000

    Revert "use tsd for typings"
    This reverts commit 3955f46b18da844087f32ad81615ec863386274e.

--- typings/tsd.d.ts
+++
@@ -1,5 +0,0 @@
-
-/// <reference path="glob/glob.d.ts" />
-/// <reference path="minimatch/minimatch.d.ts" />
-/// <reference path="node/node.d.ts" />
-/// <reference path="typescript/typescript.d.ts" />
